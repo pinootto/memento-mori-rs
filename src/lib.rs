830cdc8afb3b14d9b3648393acca9b141f64a609
Giovanni <pino_otto@yahoo.com>
2025-05-23 12:35:25 +0800

    adjusted week calculation

--- src/lib.rs
+++ src/lib.rs
@@ -31,36 +31,55 @@ impl Display for TimeUnit {
         }
     }
 }
+
+const WEEKS_IN_A_YEAR: f64 = 52.18; // roughly adjusted for leap years
+
 pub fn launch() {
     let cli = Cli::parse();
 
-    println!("{:#?}", cli);
+    // println!("{:#?}", cli);
 
     let death_date = cli.birthday + (cli.death_age as i32).years();
-    println!("Your death day is {death_date}");
+    println!("your death day: {death_date}");
 
     let birthday_timestamp = cli.birthday.in_tz("America/New_York").unwrap().timestamp();
+
+    let death_timestamp = death_date.in_tz("America/New_York").unwrap().timestamp();
 
     let now = Timestamp::now();
 
     let current_elapsed = now - birthday_timestamp;
-    println!("current elapsed = {}", current_elapsed);
+    // println!("current elapsed = {}", current_elapsed);
+
     let current_week = current_elapsed
         .total(SpanTotal::from(Unit::Week).days_are_24_hours())
-        .unwrap() as u32;
-    println!("current week in your life: {}", current_week);
+        .unwrap();
 
-    let mut week_counter = 0;
+    let life_weeks = (death_timestamp - birthday_timestamp)
+        .total(SpanTotal::from(Unit::Week).days_are_24_hours())
+        .unwrap();
+
+    println!(
+        "current week in your life: {} out of {} weeks ({} years)",
+        current_week as u16, life_weeks as u16, cli.death_age
+    );
+
+    let mut week_counter = 0.0;
+    let week_scaler = WEEKS_IN_A_YEAR / 52.0;
+    // println!("week_scaler = {}", week_scaler);
+    println!();
+    println!("year weeks");
     for year in 0..cli.death_age {
-        print!("{}  ", year);
-        for week in 0..52 {
+        print!("{:0>3}  ", year);
+        for _week in 0..52 {
             if week_counter < current_week {
                 print!("#");
             } else {
                 print!(".");
             }
-            week_counter += 1;
+            week_counter += week_scaler;
         }
         println!();
     }
+    println!("{:0>3}  ", cli.death_age);
 }
